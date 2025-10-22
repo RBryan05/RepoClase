8b8e22511d894269b4f9eaa9ac01d08a9c22c176
Alexis Rauda <126432796+RBryan05@users.noreply.github.com>
2025-10-22 10:46:17 -0600

    Implement Google Sign-In and user data handling
    Added Google Sign-In functionality with progress dialog and user data storage in Firebase.

--- app/src/main/java/com/example/chats_kotlin/OpcionesLoginActivity.kt
+++ app/src/main/java/com/example/chats_kotlin/OpcionesLoginActivity.kt
@@ -1,26 +1,48 @@
 package com.example.chats_kotlin
 
+import android.app.ProgressDialog
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.chats_kotlin.databinding.ActivityMainBinding
 import com.example.chats_kotlin.databinding.ActivityOpcionesLoginBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.database.FirebaseDatabase
 
 class OpcionesLoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityOpcionesLoginBinding
-
     private lateinit var firebaseAuth: FirebaseAuth
+
+    // INDICAMOS UN PROGRESS DIALOG
+    private lateinit var progressDialog: ProgressDialog
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityOpcionesLoginBinding.inflate(layoutInflater)
         firebaseAuth = FirebaseAuth.getInstance()
+
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Espere por favor")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
 
         ComprobarSesion()
 
@@ -35,6 +57,81 @@ class OpcionesLoginActivity : AppCompatActivity() {
         binding.opcionEmail.setOnClickListener{
             startActivity(Intent(applicationContext, LoginEmailActivity::class.java))
         }
+
+        //EVENTO INGRESAR CON GOOGLE
+        binding.opcionGoogle.setOnClickListener{
+            iniciarGoogle()
+        }
+    }
+
+    private fun iniciarGoogle() {
+        val googleSignIntent = mGoogleSignInClient.signInIntent
+        googleSignInActivityResultLauncher.launch(googleSignIntent)
+    }
+
+    private val googleSignInActivityResultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { resultado ->
+        if (resultado.resultCode == RESULT_OK) {
+            val data = resultado.data
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val cuenta = task.getResult(ApiException::class.java)
+                autenticarCuentaGoogle(cuenta.idToken)
+            } catch (e: Exception) {
+                Toast.makeText(this, "${e.message}", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            Toast.makeText(this, "Cancelado", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun autenticarCuentaGoogle(idToken: String?) {
+        val credencial = GoogleAuthProvider.getCredential(idToken, null)
+        firebaseAuth.signInWithCredential(credencial)
+            .addOnSuccessListener { authResultado ->
+                // CONDICION PARA COMPROBAR EL USER
+                if (authResultado.additionalUserInfo!!.isNewUser) {
+                    actualizarInfoUsuario()
+                } else {
+                    startActivity(Intent(this, MainActivity::class.java))
+                    finishAffinity()
+                }
+            }
+            .addOnFailureListener { e ->
+                Toast.makeText(this, "${e.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun actualizarInfoUsuario() {
+        progressDialog.setMessage("Guardando información...")
+
+        val uidU = firebaseAuth.uid
+        val nombreU = firebaseAuth.currentUser!!.displayName
+        val emailU = firebaseAuth.currentUser!!.email
+        val tiempoR = Constantes.obtenerTiempoDelD()
+
+        //ENVIAR INFORMACION A FIREBASE
+        val datosUsuarios = HashMap<String, Any>()
+        datosUsuarios["uid"] = "$uidU"
+        datosUsuarios["nombres"] = "$nombreU"
+        datosUsuarios["email"] = "$emailU"
+        datosUsuarios["tiempoR"] = "$tiempoR"
+        datosUsuarios["proveedor"] = "Google"
+        datosUsuarios["estado"] = "online"
+
+        //GUARDAMOS LA INFORMACIÓN EN FIREBASE
+        val reference = FirebaseDatabase.getInstance().getReference("Usuarios")
+        reference.child(uidU!!)
+            .setValue(datosUsuarios)
+            .addOnCompleteListener {
+                progressDialog.dismiss()
+                startActivity(Intent(application, MainActivity::class.java))
+            }
+            .addOnFailureListener{ e ->
+                progressDialog.dismiss()
+                Toast.makeText(this, "Fallo en el registro debido a ${e.message}", Toast.LENGTH_SHORT).show()
+            }
     }
 
     private fun ComprobarSesion() {
